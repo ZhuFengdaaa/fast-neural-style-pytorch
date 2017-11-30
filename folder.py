98cc1732ae78f079020fa8edd0fae8f69220a2e2
zhufengda <zhufengda@sensetime.com>
2017-11-30 15:25:35 +0800

    debug ImageFolder

--- folder.py
+++ folder.py
@@ -18,28 +18,15 @@ def is_image_file(filename):
     return any(filename_lower.endswith(ext) for ext in IMG_EXTENSIONS)
 
 
-def find_classes(dir):
-    classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
-    classes.sort()
-    class_to_idx = {classes[i]: i for i in range(len(classes))}
-    return classes, class_to_idx
-
-
-def make_dataset(dir, class_to_idx):
+def make_dataset(dir):
     images = []
-    dir = os.path.expanduser(dir)
-    for target in sorted(os.listdir(dir)):
-        d = os.path.join(dir, target)
-        if not os.path.isdir(d):
-            continue
-
-        for root, _, fnames in sorted(os.walk(d)):
-            for fname in sorted(fnames):
-                print(fname)
-                if is_image_file(fname):
-                    path = os.path.join(root, fname)
-                    item = (path, class_to_idx[target])
-                    images.append(item)
+    for root, _, fnames in sorted(os.walk(dir)):
+        for fname in sorted(fnames):
+            print(fname)
+            if is_image_file(fname):
+                path = os.path.join(root, fname)
+                item = path
+                images.append(item)
 
     return images
 
@@ -70,12 +57,8 @@ def default_loader(path):
 
 class ImageFolder(data.Dataset):
     """A generic data loader where the images are arranged in this way: ::
-        root/dog/xxx.png
-        root/dog/xxy.png
-        root/dog/xxz.png
-        root/cat/123.png
-        root/cat/nsdf3.png
-        root/cat/asd932_.png
+        root/xxx.png
+        root/xxy.png
     Args:
         root (string): Root directory path.
         transform (callable, optional): A function/transform that  takes in an PIL image
@@ -84,24 +67,20 @@ class ImageFolder(data.Dataset):
             target and transforms it.
         loader (callable, optional): A function to load an image given its path.
      Attributes:
-        classes (list): List of the class names.
-        class_to_idx (dict): Dict with items (class_name, class_index).
         imgs (list): List of (image path, class_index) tuples
     """
 
     def __init__(self, root, transform=None, target_transform=None,
                  loader=default_loader):
-        classes, class_to_idx = find_classes(root)
-        print(root, class_to_idx)
-        imgs = make_dataset(root, class_to_idx)
+        print(root)
+        imgs = make_dataset(root)
         if len(imgs) == 0:
-            raise(RuntimeError("Found 0 images in subfolders of: " + root + "\n"
-                               "Supported image extensions are: " + ",".join(IMG_EXTENSIONS)))
+            raise (RuntimeError("Found 0 images in subfolders of: " + root + "\n"
+                                                                             "Supported image extensions are: " + ",".join(
+                IMG_EXTENSIONS)))
 
         self.root = root
         self.imgs = imgs
-        self.classes = classes
-        self.class_to_idx = class_to_idx
         self.transform = transform
         self.target_transform = target_transform
         self.loader = loader
@@ -113,14 +92,10 @@ class ImageFolder(data.Dataset):
         Returns:
             tuple: (image, target) where target is class_index of the target class.
         """
-        path, target = self.imgs[index]
+        path = self.imgs[index]
         img = self.loader(path)
-        if self.transform is not None:
-            img = self.transform(img)
-        if self.target_transform is not None:
-            target = self.target_transform(target)
 
-        return img, target
+        return img
 
     def __len__(self):
         return len(self.imgs)
