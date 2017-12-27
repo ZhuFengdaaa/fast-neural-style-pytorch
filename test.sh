4478e6a9472d7601c454b4df15740649fe997612
zhufengda <zhufengda@sensetime.com>
2017-12-27 22:18:01 +0800

    run local

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python test.py --mode test --batch_size 1 --data_roots /mnt/lustre/zhufengda/300W_2016/Image --style_image styles/candy.jpg --model_path ./snapshot/generator_41000.pth
+python test.py --mode test --batch_size 1 --data_roots ./300W_2016 --style_image styles/candy.jpg --model_path ./snapshot/generator_41000.pth
--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py --mode train --data_roots /mnt/lustre/zhufengda/COCO/train2014 --gpu_ids 0 --style_image styles/candy.jpg --debug True
+python train.py --mode train --data_roots ./300W_2016 --gpu_ids 0 --style_image styles/candy.jpg --debug True
